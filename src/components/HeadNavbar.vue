993564b0dc5bb21e7007e5a28dbb1eb00ecd835e
Agung Susila Endarwanta <end.agung@gmail.com>
2019-09-24 14:12:53 +0700

    Add Home page

---
+++ src/components/HeadNavbar.vue
@@ -0,0 +1,39 @@
+<template>
+  <b-container class="header clearfix">
+    <b-navbar variant="faded" type="light">
+      <b-navbar-brand href="/">
+        <img
+          width="30px"
+          height="30px;"
+          src="../assets/logo.png"
+          class="d-inline-block align-top"
+          alt="Kitten"
+        />
+        VueApp
+      </b-navbar-brand>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item-dropdown right>
+          <template v-slot:button-content>User&nbsp;</template>
+          <b-dropdown-item href="#">Profile</b-dropdown-item>
+          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-navbar>
+  </b-container>
+</template>
+
+<script>
+import Vue from "vue";
+import bootstrap from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+Vue.use(bootstrap);
+
+export default {
+  name: "HeadNavbar"
+};
+</script>
+
+<style scoped>
+</style>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
 
 Vue.use(Router);
 
@@ -9,8 +8,10 @@ const router = new Router({
   base: process.env.BASE_URL,
   routes: [{
       path: "/",
+      alias: "/home",
       name: "home",
-      component: Home,
+      component: () =>
+        import( /* webpackChunkName: "home" */ "./views/Home.vue"),
       meta: {
         forUsers: true
       }
